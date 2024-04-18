78fe97acc590fc08a37ac8f469c644b5ade17e63
Sammywens <nwene.samuel@gmail.com>
2024-04-18 17:52:26 +0100

    user Infomation updated

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -13,7 +13,7 @@ const verifyJWT = (req, res, next) => {
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if (err) return res.status(403).json({ message: 'Forbiddenn' });
+            if (err) return res.status(403).json({ message: 'Forbidden' });
             req.user = decoded.UserInfo.username;
             req.roles = decoded.UserInfo.roles;
             next();
